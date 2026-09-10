bb547fc60d51f3fed22aa3215bd0aae6a0a39857
mosesthegreat555-ceaser <mosesthegreat555@gmail.com>
2026-09-10 21:30:00 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { MenuProvider } from './src/context/MenuContext';
+import RootNavigator from './src/navigation/RootNavigator';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <MenuProvider>
+        <StatusBar style="light" />
+        <RootNavigator />
+      </MenuProvider>
+    </SafeAreaProvider>
+  );
+}
